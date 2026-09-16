28418f67237c611ae897668c9cab93aa6b8418b7
chester <88622982+ChesterLZW@users.noreply.github.com>
2026-09-16 20:42:07 +0800

    Add interactive Duke responses

--- src/main/java/DialogBox.java
+++ src/main/java/DialogBox.java
@@ -1,4 +1,7 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -19,5 +22,25 @@ public class DialogBox extends HBox {
         this.setAlignment(Pos.TOP_RIGHT);
 
         this.getChildren().addAll(this.text, displayPicture);
+    }
+
+    /**
+     * Flips the dialog box so the image is on the left and the text is on the right.
+     */
+    private void flip() {
+        this.setAlignment(Pos.TOP_LEFT);
+        ObservableList<Node> children = FXCollections.observableArrayList(this.getChildren());
+        FXCollections.reverse(children);
+        this.getChildren().setAll(children);
+    }
+
+    public static DialogBox getUserDialog(String text, Image image) {
+        return new DialogBox(text, image);
+    }
+
+    public static DialogBox getDukeDialog(String text, Image image) {
+        DialogBox dialogBox = new DialogBox(text, image);
+        dialogBox.flip();
+        return dialogBox;
     }
 }
