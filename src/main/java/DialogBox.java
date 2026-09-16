a817aa5dd317bc6c03f10abb5d6da7b03cf52319
chester <88622982+ChesterLZW@users.noreply.github.com>
2026-09-16 20:41:24 +0800

    Build Duke chat interface

---
+++ src/main/java/DialogBox.java
@@ -0,0 +1,23 @@
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+
+public class DialogBox extends HBox {
+
+    private Label text;
+    private ImageView displayPicture;
+
+    public DialogBox(String text, Image image) {
+        this.text = new Label(text);
+        displayPicture = new ImageView(image);
+
+        this.text.setWrapText(true);
+        displayPicture.setFitWidth(100.0);
+        displayPicture.setFitHeight(100.0);
+        this.setAlignment(Pos.TOP_RIGHT);
+
+        this.getChildren().addAll(this.text, displayPicture);
+    }
+}
